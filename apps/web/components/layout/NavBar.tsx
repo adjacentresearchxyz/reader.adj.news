cf0000307a0604ebb4204f1755989ad8ed72a3e0
0xperp <0xperp@protonmail.com>
2024-05-16 17:46:51 -0400

    some updates

--- apps/web/components/layout/NavBar.tsx
+++ apps/web/components/layout/NavBar.tsx
@@ -4,14 +4,18 @@ import { SideBarWidth } from "@components/layout/SideBar";
 import * as Dialog from "@radix-ui/react-dialog";
 import clsx from "clsx";
 import { motion } from "framer-motion";
-import { useAtom } from "jotai";
 import { PanelLeft } from "lucide-react";
 import { useWindowSize } from "usehooks-ts";
+import Link from "next/link";
 
 import { trimTitle } from "../../lib/trimTitle";
 import { feedLayout } from "../../stores/ui";
 import { ThemedSkeleton } from "../ui/Skeleton";
 import { NavBarButtons } from "./NavBarButtons";
+
+// for search pulled from SideBar.tsx
+import { atom, useAtom, useAtomValue, useSetAtom } from "jotai";
+import { kmenu } from "../../stores/ui";
 
 interface NavBarTypes {
   feedId?: string;
@@ -35,6 +39,54 @@ export default function NavBar(props: NavBarTypes) {
 
   const isMobile = windowWidth < 500;
 
+  const SearchSelect = () => {
+    const setKmenu = useSetAtom(kmenu);
+
+    const handleClick = (e: React.MouseEvent) => {
+      e.preventDefault();
+      setKmenu(true);
+    };
+
+    return (
+      <Link href={"search"} shallow={true} onClick={handleClick}>
+        <Search />
+      </Link>
+    );
+  };
+
+  const Search = () => {
+    return (
+      <div className="flex flex-none cursor-pointer flex-col gap-3">
+        <div className="flex flex-1 flex-col">
+          <div className="capacitor:active:opacity-70 dark:hover:text-near-white flex flex-1 items-center space-x-1 rounded-[7px] border border-neutral-300/60 bg-white px-2 py-1 text-sm text-neutral-400 shadow-sm shadow-black/5 transition duration-150 ease-in-out hover:bg-white hover:text-neutral-500 dark:border-[#fcfcfd]/5 dark:bg-[#202022] dark:text-neutral-500 dark:shadow-black/10 dark:hover:bg-[#202022]">
+            <div className="flex-none">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  shapeRendering="geometricPrecision"
+                  d="M13.8588 13.8588C14.7183 12.9992 15.25 11.8117 15.25 10.5C15.25 7.87665 13.1234 5.75 10.5 5.75C7.87665 5.75 5.75 7.87665 5.75 10.5C5.75 13.1234 7.87665 15.25 10.5 15.25C11.8117 15.25 12.9992 14.7183 13.8588 13.8588ZM13.8588 13.8588L18.5 18.5"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                />
+              </svg>
+            </div>
+            <div className="w-0 flex-1 truncate">Search your feeds...</div>
+            <kbd className="inline-flex flex-none items-center rounded px-2 font-sans text-xs font-medium uppercase ">
+              <span className="text-neutral-400 dark:text-neutral-500/90">
+                ctrl&thinsp;k
+              </span>
+            </kbd>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <Dialog.Root>
       <div
@@ -49,22 +101,24 @@ export default function NavBar(props: NavBarTypes) {
           )}
         />
         {title == "Discover" ? (
-          <div
-            className={`flex w-full items-center overflow-x-auto scrollbar-hide`}
-          >
+          <div className={`flex w-full items-center overflow-x-auto scrollbar-hide`}>
             <motion.div
               layout="preserve-aspect"
               transition={{
                 duration: title ? 0.1 : 0,
               }}
-              className="mx-auto flex"
+              className="mx-auto flex justify-between items-center px-4 py-4 w-full"
             >
-              <div className="sm:w-[250px] md:mx-0 md:w-[500px] lg:w-[750px] xl:w-[1000px]">
-                <h1
-                  className={`-ml-1 truncate text-ellipsis font-bold tracking-[-0.01em]`}
-                >
+              <div className="hidden sm:block sm:w-[250px] md:mx-0 md:w-[350px] lg:w-[500px] xl:w-[750px]">
+                <h1 className={`-ml-1 truncate text-ellipsis font-bold tracking-[-0.01em]`}>
                   Discover
                 </h1>
+              </div>
+              <div className="flex-grow">
+                <SearchSelect />
+              </div>
+              <div className="sm:flex-grow hidden">
+                <SearchSelect />
               </div>
             </motion.div>
           </div>
@@ -77,8 +131,8 @@ export default function NavBar(props: NavBarTypes) {
                 className={clsx(
                   Layout == "Article" && "md:ml-3 md:w-[39.5em] lg:ml-6",
                   Layout == "Magazine" &&
-                    !isMobile &&
-                    "md:ml-3 md:w-[35em] lg:ml-8",
+                  !isMobile &&
+                  "md:ml-3 md:w-[35em] lg:ml-8",
                   Layout == "Card" && "lg:ml-2",
                 )}
               >
