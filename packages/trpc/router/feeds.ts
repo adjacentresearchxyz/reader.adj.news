8170c83f2ea84bb8b65fe7eeeb747201b2ecfefe
0xperp <0xperp@protonmail.com>
2024-06-06 19:29:35 -0400

    add similaity

--- packages/trpc/router/feeds.ts
+++ packages/trpc/router/feeds.ts
@@ -1,5 +1,6 @@
 import { TRPCError } from "@trpc/server";
 import { Innertube } from "youtubei.js";
+import { similarity } from 'string-similarity-js';
 import { z } from "zod";
 
 import { addFeedToUser } from "@refeed/features/discovery/addFeedToUser";
@@ -12,6 +13,33 @@ import { addFeed } from "../../features/feed/addFeed";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 // Tip - use the VSCode Outline View feature to see the APIs defined in here without having to scroll through the file
+
+async function fetchAndCombineJsonFiles() {
+  try {
+    const response = await fetch('https://data.adj.news/data/raw');
+    const data = await response.text();
+    const fileLines = data.split('\n');
+    const filteredFiles = fileLines
+      .filter(line => line.includes('"path"'))
+      .map(line => {
+        const pathStartIndex = line.indexOf('"path":"') + 8;
+        const pathEndIndex = line.indexOf('"', pathStartIndex);
+        const filePath = `https://data.adj.news/${line.substring(pathStartIndex, pathEndIndex)}`;
+        return fetch(filePath)
+          .then(response => response.json())
+          .catch(error => console.error('Error fetching JSON file:', error));
+      });
+
+    const jsonFiles = await Promise.all(filteredFiles);
+    const combinedJson = jsonFiles.reduce((acc, json) => {
+      return { ...acc, ...json };
+    }, {});
+    
+    return combinedJson;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
 
 export const feedRouter = createTRPCRouter({
   getFeedsInFolders: protectedProcedure.query(async ({ ctx }) => {
@@ -491,13 +519,24 @@ export const feedRouter = createTRPCRouter({
       return [];
     }
 
+    const allMarkets = await fetchAndCombineJsonFiles();
+
     // Convert _count to amount
-    const feeds = query.map((feed) => {
+    const feeds = query.map((feed: any) => {
       const { _count, logo_url, ...rest } = feed;
+      let relatedMarket = '';
+
+      allMarkets.forEach((market: { title: string }) => {
+        if (similarity(feed.title, market.title) > 0.8) { // Adjust the threshold as needed
+          relatedMarket = market.title;
+          console.log('Found related market! title:', feed.title, ' market:', market.title);
+        }
+      });
+
       if (logo_url) {
-        return { ...rest, amount: _count.items, logo_url: logo_url };
+        return { ...rest, amount: _count.items, logo_url: logo_url, relatedMarket: relatedMarket };
       }
-      return { ...rest, amount: _count.items, logo_url: "" };
+      return { ...rest, amount: _count.items, logo_url: "", relatedMarket: relatedMarket };
     });
 
     return feeds;
