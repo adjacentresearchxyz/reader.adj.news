dba8fa2ad853498c3deea3070db1d230f98ff4af
0xperp <0xperp@protonmail.com>
2024-07-12 11:59:29 -0400

    static gen

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -112,7 +112,7 @@ const App = ({
               <meta property="og:url" content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/" />
               <meta property="og:title" content="A Twitter for My Sister" />
               <meta property="og:description" content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling." />
-              <meta property="og:image" content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg" />
+              <meta property="og:image" content="https://adj.news/api/og?id=clyip2m1w05awmc07k4reqiln" />
             </Head>
             <ThemeProvider
               attribute="class"
